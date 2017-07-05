3170bfc6a59708d14a2f24f0d59a6e4205bd6e3c
Arjun_NA <arjunna.mm@gmail.com>
2017-07-05 22:51:37 +0530

    Add files via upload

---
+++ stemchamp-16x8-final/combinedRGB.ino
@@ -0,0 +1,13 @@
+void matrix(int M[8][16],int tmax)
+{ int R[8][16],G[8][16],B[8][16],i,j,t;
+  for(int i=0;i<8;i++)
+   for(int j=0;j<16;j++)
+   { R[i][j]=M[i][j]/100;
+     G[i][j]=(M[i][j]/10)%10;
+     B[i][j]=M[i][j]%10;
+   }
+
+  for(int t=0;t<tmax;t++)
+   gen(R,G,B);   
+}
+
---
+++ stemchamp-16x8-final/show.ino
@@ -0,0 +1,50 @@
+void show(uint16_t X[],uint16_t Y[],uint16_t Z[])
+{ uint8_t Xa[8],Xb[8],Ya[8],Yb[8],Za[8],Zb[8];
+  for(int i=0;i<8;i++)
+  {   Xa[i]=X[i]%256;
+      Xb[i]=X[i]/256;
+      Ya[i]=Y[i]%256;
+      Yb[i]=Y[i]/256;
+      Za[i]=Z[i]%256;
+      Zb[i]=Z[i]/256;  
+  }
+  int C;
+  (!upsideDown)?C=B00000001:C=B10000000;
+  for(int i=0;i<8;)
+  {
+   digitalWrite(PIN_LATCH, LOW);
+   SPI.transfer(C);
+   SPI.transfer(~Xb[i]);
+   SPI.transfer(~Yb[i]);
+   SPI.transfer(~Zb[i]);
+   SPI.transfer(C);
+   SPI.transfer(~Xa[i]);
+   SPI.transfer(~Ya[i]);
+   SPI.transfer(~Za[i]);
+   
+   digitalWrite(PIN_LATCH, HIGH);
+   if(!upsideDown){
+      if(i==6)
+      { i=1;    C=B00000010; }
+      else
+      { i+=2;   C<<=2; }
+   }
+   else
+   {  if(i==6)
+      { i=1;    C=B01000000; }
+      else
+      { i+=2;   C>>=2; }
+   }
+   }
+  digitalWrite(PIN_LATCH, LOW);
+  SPI.transfer(C);
+  SPI.transfer(~0);
+  SPI.transfer(~0);
+  SPI.transfer(~0);
+  SPI.transfer(C);
+  SPI.transfer(~0);
+  SPI.transfer(~0);
+  SPI.transfer(~0);
+  digitalWrite(PIN_LATCH, HIGH);
+  
+}
