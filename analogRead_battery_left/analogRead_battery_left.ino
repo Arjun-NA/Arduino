fd640fb7e4bfa90f93217d820f19bbf469a9cd71
Arjun_NA <arjunna.mm@gmail.com>
2017-07-05 23:19:45 +0530

    Add files via upload

---
+++ analogRead_battery_left/analogRead_battery_left.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int led=7;
+int top;
+float high, percentage;
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(led,OUTPUT);
+    lcd.begin(16, 2);
+}
+
+void loop() 
+{
+  top=analogRead(A1);
+  high=top*(5000/1023);
+  percentage=(high/4500)*100;
+  lcd.setCursor(0, 0);
+  lcd.print("Battery Left:");
+  lcd.setCursor(0, 1);
+  lcd.print(percentage);
+  lcd.print("%");
+}
