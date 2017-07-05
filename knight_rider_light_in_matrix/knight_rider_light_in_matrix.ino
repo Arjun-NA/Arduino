5aaaa1b693c375ff303d3394881560a69135e76e
Arjun_NA <arjunna.mm@gmail.com>
2017-07-05 23:05:44 +0530

    Add files via upload

---
+++ knight_rider_light_in_matrix/knight_rider_light_in_matrix.ino
@@ -0,0 +1,46 @@
+const byte PIN_LATCH = 11;
+const byte PIN_CLOCK = 12;
+const byte PIN_DATA  = 8;
+
+byte R = B11111110,G=B00000000;
+byte dir_forward = 1;
+
+void setup()
+{ 
+  
+  pinMode(PIN_LATCH, OUTPUT);
+  pinMode(PIN_CLOCK, OUTPUT);
+  pinMode(PIN_DATA , OUTPUT);
+  digitalWrite(PIN_LATCH, LOW);
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, 0);
+  digitalWrite(PIN_LATCH, HIGH);
+  
+} 
+
+void loop() 
+{ 
+  
+  digitalWrite(PIN_LATCH, LOW);
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, G);
+  
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, R);
+  if(dir_forward)
+  { 
+    R <<= 1;G>>=1;
+    R++;
+    G+=128;
+    if(127 == R)
+      dir_forward = 0;
+  } 
+  else
+  { 
+    R >>= 1; G <<= 1;
+    R+=128;
+    G++;
+    if(254 == R)
+      dir_forward = 1;
+  } 
+  digitalWrite(PIN_LATCH, HIGH);
+  delay(100);
+  
+} 
